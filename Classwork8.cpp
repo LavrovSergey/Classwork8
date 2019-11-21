f58331399726ab75bb0b2bb14ebab186f2a13804
LavrovSergey <54863977+LavrovSergey@users.noreply.github.com>
2019-11-21 18:42:36 +0200

    1

--- Classwork8.cpp
+++ Classwork8.cpp
@@ -6,54 +6,60 @@ using namespace std;
 
 struct Aeroflot
 {
-	char name[64];
-	int num;
+	char name[20];
+	char num[10];
 	char type[10];
 };
 
-void aero(Aeroflot*flot)
+void aero(Aeroflot* flot)
 {
-	for (int i = 0; i < 7; ++i) 
+	for (int i = 0; i < 7; ++i)
 	{
 		cout << i + 1 << ':';
 		cout << "Название пункта назначения: ";
-		while (flot[i].name[0] == '\0' || flot[i].name[0] < 0) 
+		while (flot[i].name[0] == '\0' || flot[i].name[0] < 0)
 		{
 			cin.getline(flot[i].name, 64);
 		}
-		while (flot[i].num <= 0/*||isalpha(flot[i].num)*/)
+		while (flot[i].num[0] == '\0' || flot[i].num[0] < 0)
 		{
-			flot[i].num = 0;
 			cout << "Номер рейса: ";
-			cin >> flot[i].num;
+			cin.getline(flot[i].num, 64);
 		}
 		cout << "Тип самолета: ";
 		while (flot[i].type[0] == '\0' || flot[i].type[0] < 0)
 		{
-		    cin.getline(flot[i].type,10);
-		} 	
+			cin.getline(flot[i].type, 10);
+		}
 		cout << endl;
 	}
 
-	char temp[64];
-	char temp2[64];
-	for (int i = 0; i < 7; ++i) 
+	char temp[20];
+	char temp1[10];
+	char temp2[10];
+	for (int i = 0; i < 7; ++i)
 	{
-		for (int j = i; j < 7; ++j) 
+		for (int j = i; j < 7; ++j)
 		{
-			if (flot[j].name[0] > flot[i].name[0]) 
+			if (flot[j].name[0] < flot[i].name[0])
 			{
-				strcpy_s(temp, flot[i].name);
-				strcpy_s(flot[i].name, flot[j].name);
-				strcpy_s(flot[j].name, temp);
-				int temp1 = flot[i].num;
-				flot[i].num = flot[j].num;
-				flot[j].num = temp1;
-				strcpy_s(temp2, flot[i].type);
-				strcpy_s(flot[i].type, flot[j].type);
-				strcpy_s(flot[j].type, temp2);
+				strcpy_s(temp, flot[j].name);
+				strcpy_s(flot[j].name, flot[i].name);
+				strcpy_s(flot[i].name, temp);
+				strcpy_s(temp1, flot[j].num);
+				strcpy_s(flot[j].num, flot[i].num);
+				strcpy_s(flot[i].num, temp1);
+				strcpy_s(temp2, flot[j].type);
+				strcpy_s(flot[j].type, flot[i].type);
+				strcpy_s(flot[i].type, temp2);
 			}
 		}
+	}
+	for (int i = 0; i < 7; i++) {
+		cout << flot[i].name;
+		cout << flot[i].num;
+		cout << flot[i].type;
+		cout << endl;
 	}
 }
 
@@ -63,7 +69,6 @@ int main()
 	Aeroflot flot[7];
 	aero(flot);
 	cout << endl;
-
 	ofstream file("Aeroflot.doc");
 	for (int i = 0; i < 7; ++i) {
 		file.write((char*)& flot[i], sizeof(Aeroflot));
@@ -71,21 +76,26 @@ int main()
 	file.close();
 
 	ifstream file1("Aeroflot.doc");
-	cout << "Введите тип самолета"<<endl;
+	cout << "Введите тип самолета" << endl;
 	char type1[10];
-	cin.getline(type1,10);
-	for (int i = 0; i < 7; ++i) {
+	int count = 0;
+	cin.getline(type1, 10);
+	for (int i = 0; i < 7; ++i)
+	{
 		file1.read((char*)& flot[i], sizeof(Aeroflot));
-		if (strcmp(type1, flot[i].type)==0) 
+		if (strcmp(type1, flot[i].type) == 0)
 		{
 			cout << endl;
-			cout << i + 1 << ':' << endl;
+			cout << i + 1 << ':';
 			cout << "   Куда: ";
 			cout << flot[i].name << endl;
 			cout << "   Номер рейса: ";
 			cout << flot[i].num << endl;
+			count++;
 		}
+
 	}
+	if (count == 0) { cout << endl << " Нет таких рейсов\n"; }
 	file1.close();
 	delete[] type1;
 	delete[] flot;
